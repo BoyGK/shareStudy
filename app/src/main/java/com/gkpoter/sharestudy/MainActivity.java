4cef455645d2c24b15725cc8a8b1c8f0ccb1bc87
BoyGK <1215356195@qq.com>
2017-05-03 17:06:23 +0800

    初始化login界面

--- app/src/main/java/com/gkpoter/sharestudy/MainActivity.java
+++ app/src/main/java/com/gkpoter/sharestudy/MainActivity.java
@@ -1,13 +1,18 @@
 package com.gkpoter.sharestudy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.gkpoter.sharestudy.ui.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
     }
 }
---
+++ app/src/main/java/com/gkpoter/sharestudy/ui/LoginActivity.java
@@ -0,0 +1,67 @@
+package com.gkpoter.sharestudy.ui;
+
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.gkpoter.sharestudy.R;
+import com.gkpoter.sharestudy.ui.fragment.LoginFragment;
+import com.gkpoter.sharestudy.ui.fragment.RegisterFragment;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity{
+
+    private ViewPager viewPager;
+
+    private Fragment[] fragments;
+    private FragmentPagerAdapter adapter;
+    private FragmentManager fragmentManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initViewPager();
+    }
+
+    private void initViewPager(){
+        viewPager= (ViewPager) findViewById(R.id.login_viewpager);
+        fragments=new Fragment[2];
+        fragmentManager=getSupportFragmentManager();
+        fragments[0]=new LoginFragment();
+        fragments[1]=new RegisterFragment();
+        adapter=new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int i) {
+                return fragments[i];
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.length;
+            }
+        };
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {}
+            @Override
+            public void onPageSelected(int i) {
+                Toast.makeText(LoginActivity.this, i + "", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onPageScrollStateChanged(int i) {}
+        });
+    }
+
+}
+
